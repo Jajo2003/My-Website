00ca6c985cfb94a3442da5930a61bb6791b185bd
jeiji <jeiji@gmail.com>
2023-06-24 19:33:00 +0400

    commit

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const home = document.querySelector('.Home');
 const Shop = document.querySelector('.Shop');
 const About = document.querySelector('.About');
-
+const main = document.getElementById('Main-content');
 function changepage(a){
   if(a==='home'){
     home.classList.add('active');
     Shop.classList.remove('active');
     About.classList.remove('active');
+    
   }
   else if(a === 'Shop'){
     Shop.classList.add('active');
     home.classList.remove('active');
     About.classList.remove('active');
+    
   }
   else{
     home.classList.remove('active');
     About.classList.add('active');
     Shop.classList.remove('active');
+    
   }
-
-
 }
